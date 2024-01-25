0c65ccbcb23e04788f839efa41fb78d0755e738e
Mark Miles <mark.miles@students.fhu.edu>
2024-01-25 12:37:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
+    #this function should do the math
     pass
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
+    #read in the integers
+    #call sum_of_products (return a value)
+    #print the result
     pass
